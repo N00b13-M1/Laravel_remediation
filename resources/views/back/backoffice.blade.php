df40ade0648c285d35977d2ae90fd5dfca30e47e
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2022-05-03 09:59:16 +0200

    Feat_backend

---
+++ resources/views/back/backoffice.blade.php
@@ -0,0 +1,9 @@
+@extends('back.layouts.app')
+@section('content')
+    @include('back.partials.navbar')
+    <div class="w-100 vh-100 d-flex justify-content-center align-items-center">
+        <div class="">
+            <h1 class="fs-1 text-center">Back-end</h1>
+        </div>
+    </div>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,10 +13,9 @@ const mix = require('laravel-mix');
 
 
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.sass('resources/sass/app.sass', 'public/css/style.css')
+    .js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css/app.css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
 ])
-.sass('resources/sass/app.scss', 'public/css')
-.sourceMaps();
